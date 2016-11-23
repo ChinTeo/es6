fa15bb1062f1d6f16a6088606eaa40a894795474
z.c <z.c.@zcs-MacBook-Pro.local>
2016-11-23 14:49:53 +0800

    :wrench:

---
+++ index.js
@@ -0,0 +1,3 @@
+let  x = `welcome to use es6!`;
+
+document.getElementById("app").innerHTML = x;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/**
+ * Created by z.c. on 16/8/14.
+ */
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './index.js')],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8181
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
